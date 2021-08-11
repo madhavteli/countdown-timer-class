4ebcc68f709acee34a8cb41e2e458290ac808ccc
mteli <mteli@xento.com>
2021-08-11 10:32:11 +0530

    class comp changes refactor

--- src/Components/Countdown.js
+++ src/Components/Countdown.js
@@ -1,23 +1,38 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
-const Countdown = (props) => {
-	let [count, setCount] = useState(props.number ? props.number : 30);
-	const decreaseCount = () => {
-		console.log(count);
-		if(count > 0){
-			count--;
-			setCount(count);
-		}else{
-			clearInterval(interval)
+class Countdown extends Component {
+	state = {
+		count: this.props.number ? this.props.number : 30
+	}
+
+	interval = null;
+
+	componentDidMount() {
+		this.interval = setInterval(() => {
+			this.setState(({ count }) => ({ count: count - 1 }));
+		}, 1000);
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+		if (nextState.count < 1) {
+			clearInterval(this.interval);
+			this.interval = null;
 		}
 	}
-	let interval = setInterval(decreaseCount, 1000);
-	return (
-		<div style={{textAlign:'center'}}>
-		<h1>{count}</h1>
-		{count === 0 && <p>Time's up!</p>}
-		</div>
-	)
+
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+
+	render() {
+		const { count } = this.state;
+		return (
+			<div style={{textAlign:'center'}}>
+				<h1>{count}</h1>
+				{count === 0 && <p>Time's up!</p>}
+			</div>
+		)
+	}
 }
 
 export default Countdown;
