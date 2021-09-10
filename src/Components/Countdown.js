bfd318d549cfb34bd0ca2b248ba0f3ad83796c67
mteli <mteli@xento.com>
2021-09-10 11:24:56 +0530

    instruction.md

--- src/Components/Countdown.js
+++ src/Components/Countdown.js
@@ -8,26 +8,26 @@ class Countdown extends Component {
 	interval = null;
 
 	componentDidMount() {
-		this.interval = setInterval(() => {
-			this.setState(({ count }) => ({ count: count - 1 }));
-		}, 1000);
+		this.interval = setInterval( () => {
+			this.setState( ( { count } ) => ({ count: count - 1 }) );
+		}, 1000 );
 	}
 
-	componentWillUpdate(nextProps, nextState) {
-		if (nextState.count < 1) {
-			clearInterval(this.interval);
+	componentWillUpdate( nextProps, nextState ) {
+		if( nextState.count < 1 ) {
+			clearInterval( this.interval );
 			this.interval = null;
 		}
 	}
 
 	componentWillUnmount() {
-		clearInterval(this.interval);
+		clearInterval( this.interval );
 	}
 
 	render() {
 		const { count } = this.state;
 		return (
-			<div style={{textAlign:'center'}}>
+			<div style={{ textAlign: 'center' }}>
 				<h1>{count}</h1>
 				{count === 0 && <p>Time's up!</p>}
 			</div>
